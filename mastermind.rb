aa7d401433dfd4abbe50437c2c47b13b1adbec19
George Hudson <georgehudson78@gmail.com>
2015-10-16 22:09:46 -0600

    WIP building working comparison

--- mastermind.rb
+++ mastermind.rb
@@ -43,36 +43,57 @@ class MasterMind
     elsif guess == ""
       p ["What, too scared to guess?"].sample
       self.evil_help
-    elsif guess == self.invalid_guess(guess)
+    elsif self.invalid_guess?(guess)
       p ["Ha! '#{guess}' is not even the correct format!"]
       self.evil_help
     else
-      self.compare_guess
+      self.compare_guess(guess)
     end
   end
 
-  def self.invalid_guess(guess)
-    guess == "la;skdjf" #need to cover nil,
+  def self.compare_guess(guess)
+    correct_color = 0
+    correct_color_and_spot = 0
+    guess_array = guess.split("")
+    guess_array.each_with_index do |color, index|
+      if color == @@secret_code[index]
+        correct_color_and_spot +=1
   end
 
-  def evil_help
+  def self.invalid_guess?(guess)
+    guess.length != 4 #need to cover for invalid letters as well
+  end
+
+  def self.evil_help
     p ["I'll explain using small words so your puny mind can understand"].sample
     p ["Merely type out four letters, with 'r' standing for Red, 'b' for Blue, 'g' for Green, and 'y', for Yellow."].sample
-    p ["Like this 'rrbg' (though that is not the answer, YOU THINK I WOULD BE FOOL ENOUGH TO GIVE YOU THE ANSWER LIKE THAT?!?)"].sample
-    p ["And then just press the 'Enter' key, if you can manage. Ahahaha!"]
+    p ["Like this #{self.not_answer} (though that is not the answer, YOU THINK I WOULD BE FOOL ENOUGH TO GIVE YOU THE ANSWER LIKE THAT?!?)"].sample
+    p ["And then just press the 'Enter' key, if you can manage. Ahahaha!"].sample
     self.turn
+  end
+
+  def self.not_answer
+    not_the_answer = []
+    4.times do
+      not_the_answer << CODE_OPTIONS.sample
+    end
+    not_the_answer
   end
 
   def self.generate_evil_mastermind_code
     @@secret_code = []
-    4 times do
+    4.times do
       @@secret_code << CODE_OPTIONS.sample
     end
+    @@reds = @@secret_code.select { |color| color == "r" }.count
+    @@blues = @@secret_code.select { |color| color == "b" }.count
+    @@greens = @@secret_code.select { |color| color == "g" }.count
+    @@yellows = @@secret_code.select { |color| color == "y" }.count
   end
 
   def self.evil_goodbye
     p ["Until Next Time. Muahahahahaha!",
-       "I win! I win! Hahaha! Foolish human! You lose!"
+       "I win! I win! Hahaha! Foolish human! You lose!",
        "You will NEVER beat me! Aaahaahahaha!"].sample
   end
 
